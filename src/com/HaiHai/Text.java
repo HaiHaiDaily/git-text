08c3606d0c25c69452039233c935e54dec962fa6
HaiHai <2362349147@qq.com>
2023-11-05 17:27:35 +0800

    fourth commit

--- src/com/HaiHai/Text.java
+++ src/com/HaiHai/Text.java
@@ -5,5 +5,6 @@ public class Text {
         System.out.println("Hello Git");
         System.out.println("我是第二个版本新加入的内容");
         System.out.println("我是第三个版本新加入的内容");
+        System.out.println("我是第四个版本新加入的内容");
     }
 }
