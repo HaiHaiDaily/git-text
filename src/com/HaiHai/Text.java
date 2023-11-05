54f67a42d529c2c2e1962a903a0884f8e6b76268
HaiHai <2362349147@qq.com>
2023-11-05 17:53:34 +0800

    fifth commit

--- src/com/HaiHai/Text.java
+++ src/com/HaiHai/Text.java
@@ -7,5 +7,6 @@ public class Text {
         System.out.println("我是第三个版本新加入的内容");
         System.out.println("我是第四个版本新加入的内容");
         System.out.println("我是hot-fix第一个版本新加入的内容");
+        System.out.println("我是第四个版本新加入的内容：push到远程库");
     }
 }
