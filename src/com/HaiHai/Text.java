2aa3d8f68b54fdd79c3a552da899f97acff8e6e1
HaiHai <2362349147@qq.com>
2023-11-05 17:18:37 +0800

    second commit

--- src/com/HaiHai/Text.java
+++ src/com/HaiHai/Text.java
@@ -3,5 +3,6 @@ package com.HaiHai;
 public class Text {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("我是第二个版本新加入的内容");
     }
 }
